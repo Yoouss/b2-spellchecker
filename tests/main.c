b0faf6611c2bf52506b0b2ce93aba04dd4fd1d98
Sofia0703 <sofia.scyeur@student.uclouvain.be>
2026-03-09 18:10:31 +0100

    Ajout du test pour la fonction word_in_dictionnary

--- tests/main.c
+++ tests/main.c
@@ -1,14 +1,13 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
-void test_function(void) {
-    CU_ASSERT(1 == 1);
-}
+
+void test_word_in_dictionary();
 
 int main(void) {
     CU_initialize_registry();
-    CU_pSuite suite = CU_add_suite("MySuite", 0, 0);
-    CU_add_test(suite, "test_function", test_function);
+    CU_pSuite suite = CU_add_suite("Détecteur d'erreurs S5", 0, 0);
+    CU_add_test(suite, "test_word_in_dictionary", test_word_in_dictionary);
     CU_basic_run_tests();
     CU_cleanup_registry();
     return 0;
