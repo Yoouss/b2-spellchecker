19d187447cfc4a230ec93871f0ea2fed1ad80b20
Younes <187375804+Yoouss@users.noreply.github.com>
2026-03-10 13:27:14 +0100

    Documentation des fonctions helpers des tests et amélioration du test words_in_file
    Fait pour la mission de s5

--- tests/detector.c
+++ tests/detector.c
@@ -5,29 +5,27 @@
 #include <stdlib.h>
 // Ajouter le include de detector.c quand il sera dans header
 
-// À enlever après implémentation
-int word_in_dictionary(char* word, Dictionary_t* dict) {
-    return 0;
-}
-
-// À enlever apres l'implémentation
-int* words_in_line(char** line, int length, Dictionary_t* dict) {
-    return NULL; 
-}
-
-void test_word_in_dictionary(void) {
+/**
+ * @brief loads the dictionary fr.dict.test using load_dictionaries()'s implementation
+ */
+Dictionary_t* load_dictionary_for_test() {
+    char* dictionaryPath = "fr.dict.test";
+   
+    // Load dictionnary
     Dictionary_t* dicts = NULL;
     size_t dicts_count = 0;
-    char* dictionaryPath = "fr.dict.test";
+
     load_dictionaries(dictionaryPath, &dicts, &dicts_count);
 
-    CU_ASSERT_PTR_NOT_NULL(dicts);
-    CU_ASSERT_EQUAL(1, word_in_dictionary("bonjour", dicts));
-    CU_ASSERT_EQUAL(0, word_in_dictionary("mmzjd", dicts));
-    CU_ASSERT_EQUAL(-1, word_in_dictionary(NULL, dicts)); // cas extrême 1 : on ne donne pas de mot à chercher
-    CU_ASSERT_EQUAL(-1, word_in_dictionary("bonjour", NULL)); // cas extrême 2 : pas de dictionnaire trouvé 
+    return dicts;
 }
 
+/**
+ * @brief Helper function of test_words_in_file and inner function of allocate_memory_for_matrix() that frees the matrix's memory
+ *
+ * @param matrix The excepted matrix representing input_5l_test.pos
+ * @param matrixLength The number of rows the matrix has
+ */
 void free_matrix_input_5l_test(int** matrix, int matrixLength) {
     for (int i = 0; i < matrixLength; i++) {
         if (matrix[i] != NULL) free(matrix[i]);
@@ -36,6 +34,15 @@ void free_matrix_input_5l_test(int** matrix, int matrixLength) {
     free(matrix);
 }
 
+/**
+ * @brief Inner function of give_expected_output_with_input_5l_test() that allocates memory depending on :
+ *
+ * @param matrix The future output of give_expected_output_with_input_5l_test()
+ * @param index The target row of the allocation
+ * @param numberOfElements The numbers of bad words on this row
+ * 
+ * @return 0 on succeed, -1 on failure after freeing the whole matrix
+ */
 int allocate_memory_for_matrix(int** matrix, int index, int numberOfElements) {
     matrix[index] = malloc(numberOfElements * sizeof(int));
 
@@ -47,6 +54,13 @@ int allocate_memory_for_matrix(int** matrix, int index, int numberOfElements) {
     return 0;
 }
 
+/**
+ * @brief Helper function of test_words_in_file() that gives the expected matrix for the file input_5l_test.txt
+ *
+ * You can find the expected positions on the file input_5l_test.pos
+ * 
+ * @return a int** matrix on succeed, NULL on failure
+ */
 int** give_expected_output_with_input_5l_test() {
     int** matrix = malloc(5 * sizeof(int *));
     if (matrix == NULL) return NULL;
@@ -74,53 +88,39 @@ int** give_expected_output_with_input_5l_test() {
     return matrix;
 }
 
+// À enlever après implémentation
+int word_in_dictionary(char* word, Dictionary_t* dict) {
+    return 0;
+}
+
+// À enlever apres l'implémentation
+int* words_in_line(char** line, int length, Dictionary_t* dict) {
+    return NULL; 
+}
+
 // A enlever après implémentation !!! 
 int** words_in_file(char** file, int length, Dictionary_t* dict ) {
     return give_expected_output_with_input_5l_test();
 }
 
+void test_word_in_dictionary(void) {
+    Dictionary_t* dicts = load_dictionary_for_test();
 
-void test_words_in_file(void) {
-    char* dictionaryPath = "fr.dict.test";
-    char* inputPath = "input_5l_test.txt";
-
-    // Load input
-    char** lines = NULL;
-    uint32_t* lines_sizes = NULL;
-    size_t line_count = 0;
-
-    read_input_file(inputPath, &lines, &lines_sizes, &line_count);
-
-    // Load dictionnary
-    Dictionary_t* dicts = NULL;
-    size_t dicts_count = 0;
-
-    load_dictionaries(dictionaryPath, &dicts, &dicts_count);
-
-    int** expectedOutput = give_expected_output_with_input_5l_test();
-    CU_ASSERT_PTR_NOT_NULL_FATAL(expectedOutput);
-
-    int** result = words_in_file(lines, line_count, dicts);
-
-    CU_ASSERT_EQUAL(result[0][0], 3);
-    CU_ASSERT_EQUAL(result[0][1], 7);
-    CU_ASSERT_EQUAL(result[1][0], 6);
-    CU_ASSERT_EQUAL(result[2][0], 4);
-    CU_ASSERT_PTR_NULL(result[3]);
-    CU_ASSERT_EQUAL(result[4][0], 4);
-
-    free_matrix_input_5l_test(expectedOutput, 5);
+    CU_ASSERT_PTR_NOT_NULL(dicts);
+    CU_ASSERT_EQUAL(1, word_in_dictionary("bonjour", dicts));
+    CU_ASSERT_EQUAL(0, word_in_dictionary("mmzjd", dicts));
+    CU_ASSERT_EQUAL(-1, word_in_dictionary(NULL, dicts)); // cas extrême 1 : on ne donne pas de mot à chercher
+    CU_ASSERT_EQUAL(-1, word_in_dictionary("bonjour", NULL)); // cas extrême 2 : pas de dictionnaire trouvé 
 }
 
+
 void test_words_in_line(void) {
-    Dictionary_t* dict = NULL;
-    size_t dict_count = 0;
-    load_dictionaries("fr.dict.test", &dict, &dict_count);
+    Dictionary_t* dicts = load_dictionary_for_test();
 
     char* line_test[] = {"manger", "une", "pomme"};//(Younes est dans le dico, "pomme" n'y est pas)
     int line_length = 3;
 
-    int* result = words_in_line(line_test, line_length, dict);
+    int* result = words_in_line(line_test, line_length, dicts);
     //"pomme" est le seul mot faux à la 3ème position
     CU_ASSERT_PTR_NOT_NULL(result);
     if (result != NULL) {
@@ -133,5 +133,37 @@ void test_words_in_line(void) {
 
     CU_ASSERT_PTR_NULL(words_in_line(line_test, line_length, NULL));// Test cas extrême : dictionnaire NULL
     
-    CU_ASSERT_PTR_NULL(words_in_line(NULL, 0, dict));// Si la ligne est vide
+    CU_ASSERT_PTR_NULL(words_in_line(NULL, 0, dicts));// Si la ligne est vide
+}
+
+void test_words_in_file(void) {
+    char* inputPath = "input_5l_test.txt";
+
+    // Load input
+    char** lines = NULL;
+    uint32_t* lines_sizes = NULL;
+    size_t line_count = 0;
+
+    read_input_file(inputPath, &lines, &lines_sizes, &line_count);
+
+    Dictionary_t* dicts = load_dictionary_for_test();
+
+    int** expectedOutput = give_expected_output_with_input_5l_test();
+    CU_ASSERT_PTR_NOT_NULL_FATAL(expectedOutput);
+
+    int** noFile = words_in_file(NULL, line_count, dicts);
+    CU_ASSERT_PTR_NULL_FATAL(noFile);
+    int** noDicts = words_in_file(lines, line_count, NULL);
+    CU_ASSERT_PTR_NULL_FATAL(noDicts);
+
+    int** result = words_in_file(lines, line_count, dicts);
+
+    CU_ASSERT_EQUAL(result[0][0], 3);
+    CU_ASSERT_EQUAL(result[0][1], 7);
+    CU_ASSERT_EQUAL(result[1][0], 6);
+    CU_ASSERT_EQUAL(result[2][0], 4);
+    CU_ASSERT_PTR_NULL(result[3]);
+    CU_ASSERT_EQUAL(result[4][0], 4);
+
+    free_matrix_input_5l_test(expectedOutput, 5);
 }
