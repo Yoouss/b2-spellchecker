af1e149f3e8a67e9f0e06bf01092fd960c5b4d41
yassineelmohajer <yassineelmohajer1522@icloud.com>
2026-03-10 15:22:51 +0100

    Implementation words_in_line et correction word_in_dictionary

--- src/detector.c
+++ src/detector.c
@@ -20,10 +20,48 @@ int word_in_dictionary(char* word, Dictionary_t* dict){
     return 0;
 }
 
+words_in_line(char** line, int length, Dictionary_t* dict){
+    if (line==NULL||dict==NULL|| length<=0){
+        return -1;
+    }
+    int count=0;
+    for(int i=0;i<length;i++){
+        if (word_in_dictionary(*line,dict)!=1){
+            count++;
+        }
+    }
+    int index[count];
+    int recount=0;
+    for(int i=0;i<length;i++){
+        if (word_in_dictionary(*line,dict)!=1){
+            index[recount]=i;
+        }
+        recount++;
+    }
+
+   return index; 
+}
 
 
 
 
 //load_dictionaries &  read_input_files
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
