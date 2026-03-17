e497eb7bd878087b2b9caf9b2bf0a6627168567d
yassineelmohajer <yassineelmohajer1522@icloud.com>
2026-03-17 16:17:22 +0100

    Ajouts de test des fonctions calculate_distance get_candidate_words

--- tests/main.c
+++ tests/main.c
@@ -4,6 +4,10 @@
 void test_word_in_dictionary();
 void test_words_in_line();
 void test_words_in_file();
+void test_get_soundex_code();
+void test_soundex();
+void test_calculate_distance();
+void test_get_candidate_words();
 
 int main(void) {
     CU_initialize_registry();
@@ -11,6 +15,12 @@ int main(void) {
     CU_add_test(suite, "test_word_in_dictionary", test_word_in_dictionary);
     CU_add_test(suite, "test_words_in_line", test_words_in_line);
     CU_add_test(suite, "test_words_in_file", test_words_in_file);
+    CU_pSuite suite = CU_add_suite("Correcteur d'erreurs S6", 0, 0);
+    CU_add_test(suite, "test de get_soundex_code", test_get_soundex_code);
+    CU_add_test(suite, "test de soundex complet", test_soundex);
+    CU_add_test(suite, "test_get_candidate_words", test_get_candidate_words);
+    CU_add_test(suite, "test_calculate_distance", test_calculate_distance);
+
     CU_basic_run_tests();
     CU_cleanup_registry();
     return 0;
