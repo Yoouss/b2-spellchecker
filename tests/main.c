e145cb0ff2f9297aff83b4aa9431ca0885fb92d7
Norman Adjovi Mélissa <adjovi.norman@student.uclouvain.be>
2026-04-06 18:55:57 +0200

    ajout de tests pour get_wrong_words_in_line

--- tests/main.c
+++ tests/main.c
@@ -6,6 +6,7 @@ void test_find_candidate_dict_for_line();
 void test_wrong_words_count_and_indexes_in_line();
 void test_scan_line_for_errors();
 void test_scan_file_for_errors();
+void test_get_wrong_words_in_line();
 
 void test_get_soundex_code(); 
 void test_soundex(); 
@@ -24,6 +25,7 @@ int main(void) {
     CU_add_test(suite_detector, "test_wrong_words_count_and_indexes_in_line", test_wrong_words_count_and_indexes_in_line);
     CU_add_test(suite_detector, "test_scan_line_for_errors", test_scan_line_for_errors);
     CU_add_test(suite_detector, "test_scan_file_for_errors", test_scan_file_for_errors);
+    CU_add_test(suite_detector, "test_get_wrong_words_in_file", test_get_wrong_words_in_line);
 
     CU_pSuite suite_corrector = CU_add_suite("Correcteur d'erreurs S6", 0, 0);
     CU_add_test(suite_corrector, "test_get_soundex_code", test_get_soundex_code);
