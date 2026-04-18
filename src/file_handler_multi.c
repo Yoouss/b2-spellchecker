c2f0b26473b996050ea4e55f63336062876dad13
Younes <187375804+Yoouss@users.noreply.github.com>
2026-04-19 00:25:16 +0200

    Mise au propre de mon code de read_input_file_multi

--- src/file_handler_multi.c
+++ src/file_handler_multi.c
@@ -15,19 +15,6 @@
 
 #define NUM_THREADS 4
 
-typedef struct {
-    char** lines;
-    uint32_t* lines_sizes;
-    size_t line_count;
-    char* file_map;
-
-    int start_index;
-    int end_index;
-} thread_data_t;
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-int status = 0;
-
 static char* map_file(const char* path, size_t* file_size) {
     int file_descriptor = open(path, O_RDONLY);
     if (file_descriptor == -1) {
@@ -74,67 +61,86 @@ static void set_line_count(const char* map, size_t file_size, size_t* line_count
     }
 }
 
-static void set_lines_sizes(const char* map, size_t file_size, uint32_t** lines_sizes) {
+static int set_lines_sizes(const char* map, size_t file_size, uint32_t** lines_sizes, size_t line_count) {
+    *lines_sizes = malloc(line_count * sizeof(uint32_t));
+    if (*lines_sizes == NULL) return -1;
+
     size_t line_index = 0;
-    size_t current_words_count = 0;
+    size_t current_chars_count = 0;
 
     for (size_t i = 0; i < file_size; i++) {
         if (map[i] == '\n') {
-            (*lines_sizes)[line_index] = current_words_count;
+            (*lines_sizes)[line_index] = current_chars_count;
 
             line_index++;
-            current_words_count = 0;
+            current_chars_count = 0;
         } 
         else {
-            current_words_count++;
+            current_chars_count++;
         }
     }
     
+    // Cas spécial où la dernière ligne n'a pas de retour à la ligne à sa fin
     if (file_size > 0 && map[file_size - 1] != '\n') {
-        (*lines_sizes)[line_index] = current_words_count;
+        (*lines_sizes)[line_index] = current_chars_count;
     }
+
+    return 0;
 }
 
-static void clean_lines(char** lines, uint32_t* sizes, size_t count) {
-    if (lines) {
-        for (size_t i = 0; i < count; i++) {
-            free(lines[i]);
+static void free_lines(char** lines, size_t line_count) {
+    for (size_t i = 0; i < line_count; i++) {
+        free(lines[i]);
+    }
+    free(lines);
+}
+
+static int set_lines(char*** lines, uint32_t* lines_sizes, size_t line_count) {
+    *lines = malloc(line_count * sizeof(char *));
+    if (*lines == NULL) return -1;
+
+    for (size_t i = 0; i < line_count; i++) {
+        size_t line_size = lines_sizes[i];
+
+        char* line = malloc((line_size + 1) * sizeof(char));
+        if (line == NULL) {
+            free_lines(*lines, i);
+            return -1;
         }
-        free(lines);
+
+        line[line_size] = '\0';
+        (*lines)[i] = line;
     }
-    if (sizes) {
-        free(sizes);
-    }
+
+    return 0;
 }
 
 int read_input_file_multi(char* input_path, char*** lines, uint32_t** lines_sizes, size_t* line_count) {
-    status = 0;
-
     size_t file_size = 0;
     char *file_map = map_file(input_path, &file_size);
     if (!file_map) return -1;
 
     set_line_count(file_map, file_size, line_count);
 
-    *lines = malloc(*line_count * sizeof(char *));
-    *lines_sizes = malloc(*line_count * sizeof(uint32_t));
-
-    if (!*lines || !*lines_sizes) {
-        clean_lines(*lines, *lines_sizes, 0);
+    if (set_lines_sizes(file_map, file_size, lines_sizes, *line_count) == -1) {
         munmap(file_map, file_size);
         return -1;
     }
 
-    set_lines_sizes(file_map, file_size, lines_sizes);
-
-    for (size_t i = 0; i < *line_count; i++) {
-        (*lines)[i] = malloc((*lines_sizes)[i] + 1);
-        (*lines)[i][(*lines_sizes)[i]] = '\0';
+    if (set_lines(lines, *lines_sizes, *line_count) == -1) {
+        free(*lines_sizes);
+        munmap(file_map, file_size);
+        return -1;
     }
 
     pthread_t threads[NUM_THREADS];
-    thread_data_t * thread_data = malloc(NUM_THREADS * sizeof(thread_data_t));
-    if (thread_data == NULL) return -1;
+    thread_data_read_input_t * thread_data = malloc(NUM_THREADS * sizeof(thread_data_read_input_t));
+    if (thread_data == NULL) {
+        free(*lines_sizes);
+        free_lines(*lines, *line_count);
+        munmap(file_map, file_size);
+        return -1;
+    }
 
     int chunk = file_size / NUM_THREADS;
 
@@ -142,8 +148,8 @@ int read_input_file_multi(char* input_path, char*** lines, uint32_t** lines_size
         thread_data[i].lines = *lines;
         thread_data[i].lines_sizes = *lines_sizes;
         thread_data[i].line_count = *line_count;
-        thread_data[i].file_map = file_map;
 
+        thread_data[i].file_map = file_map;
         thread_data[i].start_index = i * chunk;
         thread_data[i].end_index = (i == NUM_THREADS - 1) ? file_size : (i + 1) * chunk;
 
@@ -156,46 +162,62 @@ int read_input_file_multi(char* input_path, char*** lines, uint32_t** lines_size
 
     munmap(file_map, file_size);
     
-    return status;
+    return 0;
+}
+
+void set_start_line_index_and_file_offset(size_t* start_line_index, size_t* file_offset, thread_data_read_input_t *data) {
+    uint32_t* lines_sizes = data->lines_sizes;
+    size_t line_count = data->line_count;
+    int start_index = data->start_index;
+
+    // Précaution
+    *start_line_index = 0;
+    *file_offset = 0;
+
+    while (*start_line_index < line_count) {
+        int line_end_index = *file_offset + lines_sizes[*start_line_index];
+        if (line_end_index >= start_index) break;
+
+        *file_offset += lines_sizes[*start_line_index];
+
+        // Ca nous aide à éviter le cas où la dernière ligne n'a pas de \n à sa fin
+        if (*start_line_index < line_count - 1) {
+            (*file_offset)++; 
+        }
+
+        (*start_line_index)++;
+    }
 }
 
 void* read_input_file_thread(void* args) {
-    if (status == -1) pthread_exit(NULL);
-
-    thread_data_t *data = (thread_data_t *) args;
+    thread_data_read_input_t* data = (thread_data_read_input_t *) args;
 
     char** lines = data->lines;
-    uint32_t* lines_sizes = data->lines_sizes;
+
     char* file_map = data->file_map;
-    
     int start_index = data->start_index;
     int end_index = data->end_index;
 
-    int line_index = 0;
-    int total_char = 0;
+    size_t start_line_index = 0;
+    size_t file_offset = 0;
+    set_start_line_index_and_file_offset(&start_line_index, &file_offset, data);
 
-    while (total_char + lines_sizes[line_index] < start_index) {
-        total_char += lines_sizes[line_index] + 1;
-        line_index++;
-    }
+    size_t write_offset = start_index - file_offset;
 
-    int write_offset = start_index - total_char;
-
-    int current_line = line_index;
-    int current_index_in_line = write_offset;
+    size_t current_line = start_line_index;
+    size_t current_index_in_line = write_offset;
     
-    for (int i = start_index; i < end_index; i++) {
-        if (lines[current_line] != NULL) {
-            char c = file_map[i];
+    for (size_t i = start_index; i < end_index; i++) {
+        char c = file_map[i];
 
-            if (c == '\n') {
-                current_line++;
-                current_index_in_line = 0;
-            } 
-            else {
-                lines[current_line][current_index_in_line] = c;
-                current_index_in_line++;
-            }
+        if (c == '\n') {
+            current_line++;
+            current_index_in_line = 0;
+        } 
+
+        else {
+            lines[current_line][current_index_in_line] = c;
+            current_index_in_line++;
         }
     }
     
