de1ebb019d92797a8359551c760c87b3d9766c35
Sofia0703 <sofia.scyeur@student.uclouvain.be>
2026-04-21 18:42:02 +0200

    Ajout de la deuxième fonction

--- src/io.c
+++ src/io.c
@@ -12,6 +12,8 @@
 #include <dirent.h>
 
 OutputStreams_t *open_outputs(const char *pathname) {
+    if (pathname == NULL) return NULL;
+
     OutputStreams_t* output_streams = malloc(sizeof(OutputStreams_t));
     if (output_streams == NULL) return NULL;
 
@@ -25,17 +27,19 @@ OutputStreams_t *open_outputs(const char *pathname) {
     strcpy(correction_file_pathname, pathname);
     strcat(correction_file_pathname, ".fix");
     
-    int detection_file = open(detection_file_pathname, O_WRONLY|O_CREAT);
+    int detection_file = open(detection_file_pathname, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP);
     if (detection_file == -1) {
         perror("Erreur lors de l'ouverture du fichier pour la detection : "); 
-        exit(EXIT_FAILURE);
+        free(output_streams);
+        return NULL;
     }
 
-    int correction_file = open(correction_file_pathname, O_WRONLY|O_CREAT);
+    int correction_file = open(correction_file_pathname, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP);
     if (correction_file == -1) {
-        perror("Erreur lors de l'ouverture du fichier pour la correction : "); 
+        perror("Erreur lors de l'ouverture du fichier pour la correction : ");
+        free(output_streams);
         close(detection_file);
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
     output_streams->detection = detection_file;
@@ -51,7 +55,35 @@ void close_outputs(OutputStreams_t *streams) {
 
 int write_detection(OutputStreams_t *output_stream, uint32_t line_number,
                     uint32_t dict_index, uint32_t word_count, uint32_t *word_indices) {
-    // TODO
+
+    if (word_count == 0 || word_indices == NULL) return -1;
+
+    int detection_file_descriptor;
+
+    if (output_stream == NULL) detection_file_descriptor = 1;
+
+    else detection_file_descriptor = output_stream->detection;
+    
+    char* buffer = malloc(2048);
+    if (buffer == NULL) return -1;
+
+    int current_position = sprintf(buffer, "n=%d|d=%d|nb=%d|[", line_number, dict_index, word_count);
+
+    for (int i = 0; i < word_count; i++) {
+        if (i == word_count - 1) current_position += sprintf(buffer + current_position, "%d]", word_indices[i]);
+        
+        else current_position += sprintf(buffer + current_position, "%d,", word_indices[i]);
+    }
+
+    current_position += sprintf(buffer + current_position, "\n");
+
+    if (write(detection_file_descriptor, buffer, current_position) == -1) {
+        perror("Erreur lors de l'écriture de la détection : ");
+        return -1;
+    }
+   
+    free(buffer);
+
     return 0;        
 }
 
