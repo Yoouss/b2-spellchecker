df8bff56ab41459e9ae8dc34cb58b1e7ce3652fc
Sofia0703 <sofia.scyeur@student.uclouvain.be>
2026-04-21 15:26:18 +0200

    Implémentation de la première fonction de io.c

--- src/io.c
+++ src/io.c
@@ -12,8 +12,36 @@
 #include <dirent.h>
 
 OutputStreams_t *open_outputs(const char *pathname) {
-    // TODO
-    return NULL;
+    OutputStreams_t* output_streams = malloc(sizeof(OutputStreams_t));
+    if (output_streams == NULL) return NULL;
+
+    int pathname_length = strlen(pathname);
+    
+    char detection_file_pathname[pathname_length + 5];
+    strcpy(detection_file_pathname, pathname);
+    strcat(detection_file_pathname, ".err");
+
+    char correction_file_pathname[pathname_length + 5];
+    strcpy(correction_file_pathname, pathname);
+    strcat(correction_file_pathname, ".fix");
+    
+    int detection_file = open(detection_file_pathname, O_WRONLY|O_CREAT);
+    if (detection_file == -1) {
+        perror("Erreur lors de l'ouverture du fichier pour la detection : "); 
+        exit(EXIT_FAILURE);
+    }
+
+    int correction_file = open(correction_file_pathname, O_WRONLY|O_CREAT);
+    if (correction_file == -1) {
+        perror("Erreur lors de l'ouverture du fichier pour la correction : "); 
+        close(detection_file);
+        exit(EXIT_FAILURE);
+    }
+
+    output_streams->detection = detection_file;
+    output_streams->correction = correction_file;
+
+    return output_streams;
 }
 
 void close_outputs(OutputStreams_t *streams) {
