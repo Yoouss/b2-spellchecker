1ffa9a393b96f244e94bdfa07096b2647dc4ea02
Younes <187375804+Yoouss@users.noreply.github.com>
2026-06-30 18:33:17 +0200

    Added simple tests for file_handler because it was supposed to be done by someone else

--- tests/file_handler.c
+++ tests/file_handler.c
@@ -4,11 +4,23 @@
 #include <file_handler.h>
 
 void test_read_input_file(void) {
-    // TODO
-    CU_ASSERT(1==1);
+    char* input_path = "inputs/input_10l.txt";
+    char** lines = NULL;
+    uint32_t* lines_sizes = NULL;
+    size_t line_count = 0;
+
+    CU_ASSERT_EQUAL(read_input_file(input_path, &lines, &lines_sizes, &line_count), 0);
+    CU_ASSERT_EQUAL(line_count, 10);
+    CU_ASSERT_PTR_NOT_NULL(lines);
+    CU_ASSERT_PTR_NOT_NULL(lines_sizes);
 }
 
 void test_load_dictionaries(void) {
-    // TODO
-    CU_ASSERT(1==1);
+    char* dictionaries_path = "dicts";
+    Dictionary_t* dicts = NULL;
+    size_t dicts_count = 0;
+
+    CU_ASSERT_EQUAL(load_dictionaries(dictionaries_path, &dicts, &dicts_count), 0);
+    CU_ASSERT_PTR_NOT_NULL(dicts);
+    CU_ASSERT(dicts_count > 0);
 }
